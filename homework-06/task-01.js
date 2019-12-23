bc41263b93f6da9dc3d371659b952207262d5c9c
PublBox <PublBox@PublBox-MacBookPro.local>
2019-12-23 10:25:20 +0200

    update []

--- homework-06/task-01.js
+++ homework-06/task-01.js
@@ -1,5 +1,4 @@
-//------> 1
-users = [
+users=[
   {
     id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
     name: 'Moore Hensley',
@@ -24,84 +23,144 @@ users = [
     gender: 'female',
     age: 34,
   },
+  {
+    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+  },
+  {
+    id: '249b6175-5c30-44c6-b154-f120923736f5',
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    skills: ['adipisicing', 'irure', 'velit'],
+    gender: 'female',
+    age: 21,
+  },
+  {
+    id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    skills: ['ex', 'culpa', 'nostrud'],
+    gender: 'male',
+    age: 27,
+  },
+  {
+    guid: '150b00fb-dd82-427d-9faf-2879ea87c695',
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+  },
+  {
+    id: 'e1bf46ab-7168-491e-925e-f01e21394812',
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    skills: ['lorem', 'veniam', 'culpa'],
+    gender: 'female',
+    age: 39,
+  },
 ];
 
-// // # Задание 1
+// # Задание 1
 
-// const getUserNames = users => users.map(({ name }) => name);
+const getUserNames = users => users.map(({ name }) => name);
 
-// console.log(getUserNames(users));
+console.log(getUserNames(users));
 
-// // # Задание 2
+// # Задание 2
 
-// const getUsersWithEyeColor = (users, color) =>
-//   users.filter(({ eyeColor }) => eyeColor === color);
+const getUsersWithEyeColor = (users, color) =>
+users.filter(({ eyeColor }) => eyeColor === color);
 
-// console.log(getUsersWithEyeColor(users, 'blue'));
+console.log(getUsersWithEyeColor(users, 'blue'));
 
-// // # Задание 3
+// # Задание 3
 
-// const getUsersWithGender = (users, gender) =>
-//   users.filter(user => user.gender === gender).map(({ name }) => name);
+const getUsersWithGender = (users, gender) =>
+users.filter(user => user.gender === gender).map(({ name }) => name);
 
-// console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
-// // # Задание 4
+// # Задание 4
 
-// const getInactiveUsers = users => users.filter(({ isActive }) => !isActive);
+const getInactiveUsers = users => users.filter(({ isActive }) => !isActive);
 
-// console.log(getInactiveUsers(users));
+console.log(getInactiveUsers(users));
 
-// // # Задание 5
-// const getUserWithEmail = (users, email) => users.find(user => user.email === email)
+// # Задание 5
+const getUserWithEmail = (users, email) => users.find(user => user.email === email)
 
-// console.log(getUserWithEmail(users, 'moorehensley@indexia.com')); // {объект пользователя Sheree Anthony}
-// console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+console.log(getUserWithEmail(users, 'moorehensley@indexia.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
 
-// // # Задание 6
-// const getUsersWithAge = (users, min, max) =>
-//   users.filter(({ age }) => age >= min && age <= max);
+// # Задание 6
+const getUsersWithAge = (users, min, max) =>
+users.filter(({ age }) => age >= min && age <= max);
 
-// console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
-// console.log(getUsersWithAge(users, 30, 40));
-// // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
-// // # Задание 7
-// const calculateTotalBalance = users =>
-//   users.reduce((total, { balance }) => total + balance, 0);
+// # Задание 7
+const calculateTotalBalance = users =>
+users.reduce((total, { balance }) => total + balance, 0);
 
-// console.log(calculateTotalBalance(users)); // 20916
+console.log(calculateTotalBalance(users)); // 20916
 
-// // # Задание 8
+// # Задание 8
 
-// const getUsersWithFriend = (users, friendName) => {
-//   return users.reduce((names, user) => {
-//     const hasFriend = user.friends.some(item => item === friendName);
-//     return hasFriend
-//     ? [...names, user.name]
-//     : names
-//   }, []);
-// };
+const getUsersWithFriend = (users, friendName) => {
+return users.reduce((names, user) => {
+  const hasFriend = user.friends.some(item => item === friendName);
+  return hasFriend
+  ? [...names, user.name]
+  : names
+}, []);
+};
 
-// console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-// console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
-// // # Задание 9
+// # Задание 9
 
-// const getNamesSortedByFriendsCount = users => {
-//   const sortUsers = [ ...users ].sort((a,b) => a.friends.length - b.friends.length);
-//   return sortUsers.map(({name}) => name);
-// };
+const getNamesSortedByFriendsCount = users => {
+const sortUsers = [ ...users ].sort((a,b) => a.friends.length - b.friends.length);
+return sortUsers.map(({name}) => name);
+};
 
-// console.log(getNamesSortedByFriendsCount(users));
+console.log(getNamesSortedByFriendsCount(users));
 
 // # Задание 10
 
 const getSortedUniqueSkills = users => {
-  return users.map(({ skills }) => skills)
-    .flat()
-    .filter((item, index, arr) => arr.indexOf(item) === index)
-    .sort();
+return users.map(({ skills }) => skills)
+  .flat()
+  .filter((item, index, arr) => arr.indexOf(item) === index)
+  .sort();
 };
 console.log(getSortedUniqueSkills(users));
